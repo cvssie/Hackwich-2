f034969d53809a5b439986c26140eb1e51b57ce8
Cassandra K <ckk30@hawaii.edu>
2023-09-05 14:16:23 -1000

    finished part 8 and added the 2nd button

--- Hackwich 2/ViewController.swift
+++ Hackwich 2/ViewController.swift
@@ -8,17 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //part 8 declare 3 string variables
+    var firstString = "If im the first one"
+    
+    var secondString = " then that means im next"
+    
+    var thirdString = " "
+    
     override func viewDidLoad() {
+        thirdString = firstString + secondString
+        
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view.
     }
-
-
-    @IBAction func pressmebuttonpressed(_ sender: Any)
-    {
+    
+    
+    
+    @IBAction func pressmebutton(_ sender: Any) {
         //code block for IBAction
-      print("Hello World")
+        print("Hello World")
+    }
+    
+    
+    
+    
+    @IBAction func Button2(_ sender: Any) {
+        print(thirdString)
         
     }
 }
