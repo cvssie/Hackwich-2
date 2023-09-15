83b282de33ebf38fc6fd2c2c8d53136f21699972
Cassandra K <ckk30@hawaii.edu>
2023-09-14 14:13:16 -1000

    Created the IBAction for the background color to change to blue

---
+++ Hackwich 2/secondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  secondViewController.swift
+//  Hackwich 2
+//
+//  Created by Cassie Kauhane on 9/14/23.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    @IBAction func changeColorButtonPressed(_ sender: Any) {
+        self.view.backgroundColor = UIColor.blue
+        
+    }
+    
+    
+}
